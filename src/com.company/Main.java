6e9a022d05608d32a994908f37bdd5a1c1b6a3bd
Egor Chernook <egor.chernook@yandex.ru>
2021-09-22 03:07:27 +0600

    added interface Addable<> added interface Deletable<> added interface Findable<> added class UserCollection implements Addable<User>, Deletable<User>,Findable<User>

--- src/com.company/Main.java
+++ src/com.company/Main.java
@@ -2,6 +2,8 @@ package com.company;
 
 import people.*;
 
+import java.util.Iterator;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -16,5 +18,10 @@ public class Main {
 
         User user = new User( "Ivan", "88005553535", 1);
         System.out.println( user );
+
+        UserCollection userCollection = new UserCollection();
+        userCollection.add( user );
+        userCollection.add( ( new User("John", "89547286518", 2)));
+        System.out.println( userCollection );
     }
 }
---
+++ src/people/Addable.java
@@ -0,0 +1,5 @@
+package people;
+
+public interface Addable<Type> {
+    void add( Type element);
+}
