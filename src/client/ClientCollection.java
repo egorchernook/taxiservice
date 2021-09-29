f021717f0c453313b06eeecda23860ada93fc465
EShafeev <Asegah29@gmail.com>
2021-09-29 22:08:27 +0600

    test

---
+++ src/client/ClientCollection.java
@@ -0,0 +1,13 @@
+package client;
+
+import java.util.*;
+import controllers.*;
+
+
+public class ClientCollection {
+        implements Addable<>,
+        Deletable<>,
+        Findable<> {
+    private Map<Long,> = new HashMap<>();
+    }
+}
